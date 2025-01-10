366bf2c9e04d4fc62f57335d6d13dd7ba40a5e0d
Rene <Rene.Mlodoch@gmail.com>
2025-01-10 09:26:03 +0100

    Update cocktails.py

--- barroboter/cocktails.py
+++ barroboter/cocktails.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+class Cocktail:
+    def __init__(self):
+        self.cocktails = ["Mojito", "Martini", "Margarita"]
+
+    def select(self, cocktail):
+        conn = sqlite3.connect('barrobot.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM cocktails WHERE name = ?', (cocktail,))
+        cocktail_data = cursor.fetchone()
+        conn.close()
+        if cocktail_data:
+            return {
+                "name": cocktail_data[1],
+                "ingredients": cocktail_data[2],
+                "total_volumes": cocktail_data[3],
+                "pour_times": cocktail_data[4],
+                "image_url": cocktail_data[5]
+            }
+        else:
+            return "Cocktail not found", 404
