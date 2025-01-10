c9b358fb3c8770ccadf2f221fa7858582caa7157
Rene <Rene.Mlodoch@gmail.com>
2025-01-10 09:25:53 +0100

    Update scale.py

--- barroboter/scale.py
+++ barroboter/scale.py
@@ -1,1 +1,23 @@
+class Scale:
+    def __init__(self):
+        self.enabled = True
 
+    def init(self):
+        if self.enabled:
+            return "Scale initialized"
+        else:
+            return "Scale is disabled"
+
+    def calibrate(self):
+        if self.enabled:
+            return "Scale calibrated"
+        else:
+            return "Scale is disabled"
+
+    def enable(self):
+        self.enabled = True
+        return "Scale enabled"
+
+    def disable(self):
+        self.enabled = False
+        return "Scale disabled"
