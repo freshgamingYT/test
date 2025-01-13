196c5cd6b3a6a8142b5b2e4ffce4b4175454b19e
Rene <Rene.Mlodoch@gmail.com>
2025-01-13 11:17:35 +0100

    Update app.py

--- code/python/app.py
+++ code/python/app.py
@@ -7,8 +7,8 @@ import sys
 import json
 import os
 
-from logger.logger import setup_logger
-from routes import register_routes
+from python.logger.logger import setup_logger
+from python.routes import register_routes
 
 class App:
     def __init__(self, config_file: str):
@@ -42,7 +42,7 @@ class App:
 
 
 if __name__ == '__main__':
-    config_file = os.path.join(os.path.dirname(__file__), '../config/config.json')
+    config_file = os.path.join(os.path.dirname(__file__), '../files/config.json')
     config = App(config_file=config_file)
     app_instance = config.create_app()
 
