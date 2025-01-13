27d9c2023ee3c15f0610bbc2832327f68481b3a3
Rene <Rene.Mlodoch@gmail.com>
2025-01-13 11:46:07 +0100

    Update routes.py

--- code/python/routes.py
+++ code/python/routes.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request, jsonify
-from stepper.positions import PositionManager
+from flask import Blueprint, render_template, request, jsonify, send_from_directory
+from .stepper.positions import PositionManager
+import logging
+import os
+
+logger = logging.getLogger('my_logger')
 
 cocktail_route = Blueprint('cocktail_route', __name__)
 main_route = Blueprint('main_route', __name__)
@@ -8,11 +12,13 @@ position_manager = PositionManager('../files/positions.json')
 
 @cocktail_route.route('/1', methods=['GET'])
 def cocktail():
+    logger.debug('Serving C1.html')
     return render_template('C1.html')
 
 @cocktail_route.route('/start', methods=['POST'])
 def start():
     data = request.get_json()
+    logger.debug(f'Received start button press: {data}')
     if data['button'] == 'start':
         position_manager.load_positions()
         return jsonify({'status': 'success'})
@@ -20,8 +26,13 @@ def start():
 
 @main_route.route('/', methods=['GET'])
 def index():
+    logger.debug('Serving index.html')
     return render_template('index.html')
+
+@main_route.route('/pictures/<path:filename>')
+def pictures(filename):
+    return send_from_directory(os.path.join(os.path.dirname(__file__), '../pictures'), filename)
 
 def register_routes(app):
     app.register_blueprint(cocktail_route, url_prefix="/cocktail")
-    app.register_blueprint(main_route, url_prefix="/")
+    app
